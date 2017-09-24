c70f8ff4e10ee8e760c3002a981a3c65ace373bf
StevePro7 <steveprozune@hotmail.com>
2017-09-24 22:09:59 +0100

    Complete main

--- dev/main.c
+++ dev/main.c
@@ -1,14 +1,57 @@
 #include "main.h"
 
-void main (void)
+void main()
 {
-	SMS_init();
-	//SMS_setSpritePaletteColor(0, RGB(0,0,0));
-	SMS_setSpritePaletteColor(0, RGB(2,1,0));
-	SMS_displayOn();
-	for (;;)
+	InitConsole();
+
+	gamelevel=0;
+	gamestock=3;
+	
+	while(1)
 	{
-		SMS_waitForVBlank();
+		// Testing
+		/*
+		stagenum=8;
+		numplayers=6;
+		InitPlayerConstants();
+		InitPlayer();
+		InitPlayStage();
+		*/
+			
+		// Mikgames
+		InitMikGamesStage();
+		
+		// Game intro
+		InitIntroStage(1);
+		InitIntroStage(4);
+		InitIntroStage(2);
+		
+		// Logo
+		InitLogoStage();		
+
+		// If we have jukebox before play
+		if(dojukebox==1)
+			InitJukeboxStage();
+		
+		// Selector
+		InitSelectorStage();
+		
+		// Init stage and players
+		stagenum=1;
+		numplayers=gamestock;
+
+		// Stage looping
+		while(stagenum<10)
+		{
+			if(stagenum==0)
+				InitGameoverStage();
+			else if(stagenum==9)
+			{
+				InitIntroStage(5);
+				InitIntroStage(3);
+			}
+			else InitPlayStage();
+		}
 	}
 }
 
