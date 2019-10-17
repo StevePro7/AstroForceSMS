82cbda340b7b944c8988940756db3d60d0229b5e
StevePro7 <steveprozune@hotmail.com>
2019-10-17 16:59:16 +0100

    no message

--- dev/build.bat
+++ dev/build.bat
@@ -12,64 +12,6 @@ REM bank*.bat
 REM gfx.bat
 REM psg.bat
 
-cd engine
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 asm_manager.c
-cd ..
-
-cd source
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 _sms_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 _snd_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 global_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 debug_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 hack_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 level_object.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 anim_object.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 audio_object.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 intro_object.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 tutorial_object.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 content_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 font_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 text_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 delay_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 reset_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 sprite_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 tile_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 memo_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 level_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 anim_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 input_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 player_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 enemy_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 score_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 stats_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 audio_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 game_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 state_manager.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 none_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 splash_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 intro_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 begin_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 title_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 entry_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 diff_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 pace_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 level_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 view_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 init_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 load_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 ready_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 play_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 reset_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 pass_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 gems_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 dead_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 cont_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 over_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 beat_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 test_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 func_screen.c
-REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 screen_manager.c
-cd ..
 
 REM echo Build main
 sdcc -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
@@ -94,104 +36,9 @@ echo.
 
 
 REM echo Linking
-sdcc -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
--Wl-b_BANK2=0x8000 ^
--Wl-b_BANK3=0x8000 ^
--Wl-b_BANK4=0x8000 ^
--Wl-b_BANK5=0x8000 ^
--Wl-b_BANK6=0x8000 ^
--Wl-b_BANK7=0x8000 ^
--Wl-b_BANK8=0x8000 ^
--Wl-b_BANK9=0x8000 ^
--Wl-b_BANK10=0x8000 ^
--Wl-b_BANK11=0x8000 ^
--Wl-b_BANK12=0x8000 ^
--Wl-b_BANK13=0x8000 ^
--Wl-b_BANK14=0x8000 ^
--Wl-b_BANK15=0x8000 ^
-..\crt0\crt0_sms.rel main.rel ^
-..\lib\SMSlib.lib ^
-..\lib\PSGlib.rel ^
-banks\bank2.rel ^
-banks\bank3.rel ^
-banks\bank4.rel ^
-banks\bank5.rel ^
-banks\bank6.rel ^
-banks\bank7.rel ^
-banks\bank8.rel ^
-banks\bank9.rel ^
-banks\bank10.rel ^
-banks\bank11.rel ^
-banks\bank12.rel ^
-banks\bank13.rel ^
-banks\bank14.rel ^
-banks\bank15.rel ^
-engine\asm_manager.rel ^
-source\_sms_manager.rel ^
-source\_snd_manager.rel ^
-source\global_manager.rel ^
-source\debug_manager.rel ^
-source\hack_manager.rel ^
-source\level_object.rel ^
-source\anim_object.rel ^
-source\audio_object.rel ^
-source\intro_object.rel ^
-source\tutorial_object.rel ^
-source\content_manager.rel ^
-source\font_manager.rel ^
-source\text_manager.rel ^
-source\delay_manager.rel ^
-source\reset_manager.rel ^
-source\sprite_manager.rel ^
-source\tile_manager.rel ^
-source\memo_manager.rel ^
-source\level_manager.rel ^
-source\anim_manager.rel ^
-source\input_manager.rel ^
-source\player_manager.rel ^
-source\enemy_manager.rel ^
-source\score_manager.rel ^
-source\state_manager.rel ^
-source\stats_manager.rel ^
-source\audio_manager.rel ^
-source\game_manager.rel ^
-source\none_screen.rel ^
-source\splash_screen.rel ^
-source\intro_screen.rel ^
-source\begin_screen.rel ^
-source\title_screen.rel ^
-source\entry_screen.rel ^
-source\diff_screen.rel ^
-source\pace_screen.rel ^
-source\level_screen.rel ^
-source\init_screen.rel ^
-source\ready_screen.rel ^
-source\load_screen.rel ^
-source\play_screen.rel ^
-source\reset_screen.rel ^
-source\pass_screen.rel ^
-source\gems_screen.rel ^
-source\dead_screen.rel ^
-source\cont_screen.rel ^
-source\over_screen.rel ^
-source\beat_screen.rel ^
-source\test_screen.rel ^
-source\func_screen.rel ^
-source\view_screen.rel ^
-source\screen_manager.rel ^
-gfx.rel ^
-psg.rel
-
-REM echo Binary output
+sdcc -o output.ihx -mz80 --data-loc 0xC000 --no-std-crt0 crt0_sms.rel main.rel SMSlib.lib
 ihx2sms output.ihx output.sms
 
-REM copy output.sms
-copy output.sms ..
-copy output.sms ..\PlatformExplorer-3.1.sms
-REM copy output.sms ..\asm
-REM cd ..\asm
-REM smsexamine.exe output.sms
-REM cd ..\dev
 
 REM https://www.askingbox.com/question/batch-script-delete-file-if-it-exists
 cd engine
@@ -215,6 +62,4 @@ REM if exist "*.rel" del "*.rel" > nul
 if exist "*.sym" del "*.sym" > nul
 
 
-REM C:\SEGA\Fusion\fusion.exe output.sms
-REM C:\SEGA\Meka\mekaw.exe output.sms
-java -jar C:\SEGA\Emulicious\emulicious.jar output.sms
+C:\SEGA\Fusion\fusion.exe output.sms
--- dev/main.c
+++ dev/main.c
@@ -1,61 +1,14 @@
-#include "main.h"
+#include "SMSlib.h"
 
-void main(void)
+void main( void )
 {
-	// Global variables.
-	static bool global_pause;
-	unsigned char open_screen_type;
-
-	engine_asm_manager_clear_VRAM();
-	devkit_SMS_init();
-	devkit_SMS_displayOff();
-
-	devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
-	devkit_SMS_useFirstHalfTilesforSprites_False();
-	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
-
-	engine_content_manager_load_back_tiles();
-	engine_content_manager_load_sprites();
-
-	open_screen_type = screen_type_splash;
-	//open_screen_type = screen_type_intro;
-	//open_screen_type = screen_type_begin;
-	//open_screen_type = screen_type_init;
-
-	engine_state_manager_init();
-	engine_screen_manager_init( open_screen_type );
-	devkit_SMS_displayOn();
-	for (;;)
+	SMS_setSpritePaletteColor( 0, RGB( 3, 3, 3 ) );
+	SMS_displayOn();
+	for( ;;)
 	{
-		if( devkit_SMS_queryPauseRequested() )
-		{
-			devkit_SMS_resetPauseRequest();
-			global_pause = !global_pause;
-			if( global_pause )
-			{
-				devkit_PSGSilenceChannels();
-			}
-			else
-			{
-				devkit_PSGRestoreVolumes();
-			}
-		}
-
-		if( global_pause )
-		{
-			continue;
-		}
-
-		devkit_SMS_initSprites();
-		engine_input_manager_update();
-
-		engine_screen_manager_update();
-
-		devkit_SMS_finalizeSprites();
-		devkit_SMS_waitForVBlank();
-		devkit_SMS_copySpritestoSAT();
-
-		devkit_PSGFrame();
-		devkit_PSGSFXFrame();
+		SMS_waitForVBlank();
 	}
 }
+
+SMS_EMBED_SEGA_ROM_HEADER( 9999, 0 );
+SMS_EMBED_SDSC_HEADER( 1, 0, 2017, 9, 15, "StevePro Studios", "Hello World", "Simple Sega Master System demo to run on real hardware!" );
