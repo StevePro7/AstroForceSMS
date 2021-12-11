14ff4b1e1c315f8b8ed66a881861c31c478278c2
StevePro7 <steveprozune@hotmail.com>
2021-12-11 18:17:06 +0000

    no message

--- dev/build.bat
+++ dev/build.bat
@@ -70,4 +70,4 @@ if exist "*.noi" del "*.noi" > nul; if exist "*.sym" del "*.sym" > nul; if exist
 :: Run
 ::java -jar E:\SEGA\Emulicious\Emulicious.jar output.sms
 ::C:\SEGA\meka\mekaw.exe output.sms
-::output.sms
+output.sms
