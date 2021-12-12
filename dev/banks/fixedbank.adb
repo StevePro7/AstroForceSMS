1b34084d3e01a2d449c21a4e628f1deb4f5b093c
StevePro7 <steveprozune@hotmail.com>
2021-12-12 21:32:49 +0000

    no message

--- dev/banks/fixedbank.adb
+++ dev/banks/fixedbank.adb
@@ -1,5 +1,6 @@
 M:fixedbank
 T:Ffixedbank$enemy[({0}S:S$enemyposx$0$0({1}SC:U),Z,0,0)({1}S:S$enemyposy$0$0({1}SC:U),Z,0,0)({2}S:S$enemytype$0$0({1}SC:U),Z,0,0)({3}S:S$enemyframe$0$0({1}SC:U),Z,0,0)({4}S:S$enemyparama$0$0({1}SC:U),Z,0,0)({5}S:S$enemyparamb$0$0({1}SC:U),Z,0,0)({6}S:S$enemyenergy$0$0({1}SC:U),Z,0,0)({7}S:S$enemywidth$0$0({1}SC:U),Z,0,0)({8}S:S$enemyheight$0$0({1}SC:U),Z,0,0)]
 S:G$palette_bin$0$0({16}DA16d,SC:U),D,0,0
+S:G$sinustable$0$0({256}DA256d,SC:U),D,0,0
 S:G$updatestagefunctions$0$0({2}DA1d,DC,DF,SV:S),D,0,0
 S:G$intro1_psg$0$0({2369}DA2369d,SC:U),D,0,0
