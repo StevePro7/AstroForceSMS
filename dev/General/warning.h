67348c803b6257dd7aa50d9f7f5f369272d6e387
StevePro7 <steveprozune@hotmail.com>
2021-12-14 21:43:09 +0000

    no message

--- dev/General/warning.h
+++ dev/General/warning.h
@@ -1,9 +1,11 @@
 #ifndef _WARNING_H_
 #define _WARNING_H_
 
+#include "../defines.h"
+
 void warning_foo();
 
-//void InitWarning( enemy *en );
-//unsigned char UpdateWarning( enemy *en );
+void InitWarning( enemy *en );
+unsigned char UpdateWarning( enemy *en );
 
 #endif//_WARNING_H_
