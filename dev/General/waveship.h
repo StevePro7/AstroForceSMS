e5465b25baa6d93675cc0d12aa52dfa7f403c5cd
StevePro7 <steveprozune@hotmail.com>
2021-12-19 21:54:20 +0000

    no message

--- dev/General/waveship.h
+++ dev/General/waveship.h
@@ -1,9 +1,10 @@
 #ifndef _WAVESHIP_H_
 #define _WAVESHIP_H_
 
-void waveship_foo();
+#include "../defines.h"
 
-//void InitWaveShip( enemy *en );
-//unsigned char UpdateWaveShip( enemy *en );
+// ADRIANA
+void InitWaveShip( enemy *en );
+unsigned char UpdateWaveShip( enemy *en );
 
 #endif//_WAVESHIP_H_
