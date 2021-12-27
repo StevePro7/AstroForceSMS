faea4199e011c2d90112e767dcfb516010620d05
StevePro7 <steveprozune@hotmail.com>
2021-12-27 18:15:15 +0000

    no message

--- dev/Stages/stage3laser.h
+++ dev/Stages/stage3laser.h
@@ -1,9 +1,10 @@
 #ifndef _STAGE3LASER_H_
 #define _STAGE3LASER_H_
 
-void stage3laser_foo();
+#include "../defines.h"
 
-//void InitStage3Laser( enemy *en );
-//unsigned char UpdateStage3Laser( enemy *en );
+// ADRIANA
+void InitStage3Laser( enemy *en );
+unsigned char UpdateStage3Laser( enemy *en );
 
 #endif//_STAGE3LASER_H_
--- dev/Stages/stage8lateral.h
+++ dev/Stages/stage8lateral.h
@@ -1,9 +1,10 @@
 #ifndef _STAGE8LATERAL_H_
 #define _STAGE8LATERAL_H_
 
-void stage8lateral_foo();
+#include "../defines.h"
 
-//unsigned char UpdateStage8Lateral( enemy *en );
-//void InitStage8Lateral( enemy *en );
+// ADRIANA
+unsigned char UpdateStage8Lateral( enemy *en );
+void InitStage8Lateral( enemy *en );
 
 #endif//_STAGE8LATERAL_H_
