aa907236eb577b2d7475beda41231d227116e337
StevePro7 <steveprozune@hotmail.com>
2021-12-27 18:53:23 +0000

    no message

--- dev/Stages/stage6endbossb.h
+++ dev/Stages/stage6endbossb.h
@@ -1,11 +1,12 @@
 #ifndef _STAGE6ENDBOSSB_H_
 #define _STAGE6ENDBOSSB_H_
 
-void stage6endbossb_foo();
+#include "../defines.h"
 
-//void UpdateStage6EndBossB0( enemy *en );
-//void UpdateStage6EndBossB1( enemy *en );
-//void InitStage6EndBossB( enemy *en );
-//unsigned char UpdateStage6EndBossB( enemy *en );
+// ADRIANA
+void UpdateStage6EndBossB0( enemy *en );
+void UpdateStage6EndBossB1( enemy *en );
+void InitStage6EndBossB( enemy *en );
+unsigned char UpdateStage6EndBossB( enemy *en );
 
 #endif//_STAGE6ENDBOSSB_H_
