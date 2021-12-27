f2a36feddb818d59ba1ff5e618c9b2dd63091ac5
StevePro7 <steveprozune@hotmail.com>
2021-12-27 18:41:42 +0000

    no message

--- dev/Stages/stage3endboss.h
+++ dev/Stages/stage3endboss.h
@@ -1,11 +1,12 @@
 #ifndef _STAGE3ENDBOSS_H_
 #define _STAGE3ENDBOSS_H_
 
-void stage3endboss_foo();
+#include "../defines.h"
 
-//void DoStage3EndBossCrossShooting( enemy *en );
-//void UpdateStage3EndBoss2( enemy *en );
-//void UpdateStage3EndBoss1( enemy *en );
-//unsigned char UpdateStage3EndBoss( enemy *en );
+// ADRIANA
+void DoStage3EndBossCrossShooting( enemy *en );
+void UpdateStage3EndBoss2( enemy *en );
+void UpdateStage3EndBoss1( enemy *en );
+unsigned char UpdateStage3EndBoss( enemy *en );
 
 #endif//_STAGE3ENDBOSS_H_
--- dev/Stages/stage4.h
+++ dev/Stages/stage4.h
@@ -1,9 +1,8 @@
 #ifndef _STAGE4_H_
 #define _STAGE4_H_
 
-void stage4_foo();
-
-//void UpdateStage4();
-//void InitStage4();
+// ADRIANA
+void UpdateStage4();
+void InitStage4();
 
 #endif//_STAGE4_H_
