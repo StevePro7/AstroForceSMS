00006effc4de5c695dbaf4faa2751e253f7fbb24
StevePro7 <steveprozune@hotmail.com>
2021-12-27 19:00:24 +0000

    no message

--- dev/Stages/stage7endboss.h
+++ dev/Stages/stage7endboss.h
@@ -1,12 +1,13 @@
 #ifndef _STAGE7ENDBOSS_H_
 #define _STAGE7ENDBOSS_H_
 
-void stage7endboss_foo();
+#include "../defines.h"
 
-//void InitStage7EndBossB( enemy *en );
-//void InitStage7EndBoss( enemy *en );
-//void DoWormMovement( enemy *en );
-//unsigned char UpdateStage7EndBoss( enemy *en );
-//unsigned char UpdateStage7EndBossB( enemy *en );
+// ADRIANA
+void InitStage7EndBossB( enemy *en );
+void InitStage7EndBoss( enemy *en );
+void DoWormMovement( enemy *en );
+unsigned char UpdateStage7EndBoss( enemy *en );
+unsigned char UpdateStage7EndBossB( enemy *en );
 
 #endif//_STAGE7ENDBOSS_H_
