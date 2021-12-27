b843e9cb186884369b552849b3e6cff88766838a
StevePro7 <steveprozune@hotmail.com>
2021-12-27 18:51:54 +0000

    no message

--- dev/Stages/stage5missile.h
+++ dev/Stages/stage5missile.h
@@ -1,8 +1,9 @@
 #ifndef _STAGE5MISSILE_H_
 #define _STAGE5MISSILE_H_
 
-void stage5missile_foo();
+#include "../defines.h"
 
-//unsigned char UpdateStage5Missile( enemy *en );
+// ADRIANA
+unsigned char UpdateStage5Missile( enemy *en );
 
 #endif//_STAGE5MISSILE_H_
--- dev/Stages/stage6.h
+++ dev/Stages/stage6.h
@@ -1,9 +1,8 @@
 #ifndef _STAGE6_H_
 #define _STAGE6_H_
 
-void stage6_foo();
-
-//void InitStage6();
-//void UpdateStage6();
+// ADRIANA
+void InitStage6();
+void UpdateStage6();
 
 #endif//_STAGE6_H_
