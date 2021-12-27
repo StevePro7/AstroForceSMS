d3bc6be129cf009d5cc6767901dfa65aeed55857
StevePro7 <steveprozune@hotmail.com>
2021-12-27 18:23:40 +0000

    no message

--- dev/Stages/stage8.h
+++ dev/Stages/stage8.h
@@ -1,9 +1,8 @@
 #ifndef _STAGE8_H_
 #define _STAGE8_H_
 
-void stage8_foo();
-
-//void UpdateStage8();
-//void InitStage8();
+// ADRIANA
+void UpdateStage8();
+void InitStage8();
 
 #endif//_STAGE8_H_
